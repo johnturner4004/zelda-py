d1b8d62a1d971ec8091f01982f58904e502098b5
John Turner <johnturner4004@gmail.com>
2023-06-09 08:25:32 -0500

    feat: health and energy bars

--- code/ui.py
+++ code/ui.py
@@ -3,7 +3,32 @@ from settings import *
 
 class UI:
   def __init__(self):
-    pass
+    
+    # general
+    self.display_surface = pygame.display.get_surface()
+    self.font = pygame.font.Font(UI_FONT, UI_FONT_SIZE)
+
+    # bar setup
+    self.health_bar_rect = pygame.Rect(10, 10, HEALTH_BAR_WIDTH, BAR_HEIGHT)
+    self.energy_bar_rect = pygame.Rect(10, 34, ENERGY_BAR_WIDTH, BAR_HEIGHT)
+
+  def show_bar(self, cur_amount, max_amount, bg_rect, color):
+    # draw bg
+    pygame.draw.rect(self.display_surface, UI_BG_COLOR, bg_rect)
+
+    # convert stat to pixels
+    ratio = cur_amount / max_amount
+    cur_width = bg_rect.width * ratio
+    cur_rect = bg_rect.copy()
+    cur_rect.width = cur_width
+
+    # draw the bar
+    pygame.draw.rect(self.display_surface, color, cur_rect)
+    pygame.draw.rect(self.display_surface, UI_BORDER_COLOR, bg_rect, 3)
 
   def display(self, player):
-    pass
+    self.show_bar(player.health, player.stats['health'], self.health_bar_rect, HEALTH_COLOR)
+    self.show_bar(player.energy, player.stats['energy'], self.energy_bar_rect, ENERGY_COLOR)
+
+    # used for initial testing
+    # pygame.draw.rect(self.display_surface, 'black', self.health_bar_rect)
