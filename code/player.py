762496889e711d0be4b1ea1e7d5836b7937dcc19
John Turner <johnturner4004@gmail.com>
2023-06-12 11:03:39 -0500

    refactor: move shared classes from player to entity

--- code/player.py
+++ code/player.py
@@ -1,8 +1,9 @@
 import pygame
 from settings import *
 from support import import_folder
+from entity import Entity
 
-class Player(pygame.sprite.Sprite):
+class Player(Entity):
   def __init__(self, pos, groups, obstacle_sprites, create_attack, destroy_attack, create_magic):
     super().__init__(groups)
     self.image = pygame.image.load('../graphics/test/player.png').convert_alpha()
@@ -145,34 +146,6 @@ class Player(pygame.sprite.Sprite):
     else:
       if 'attack' in self.status:
         self.status = self.status.replace('_attack', '')
-
-  def move(self, speed):
-    if self.direction.magnitude() != 0:
-      self.direction = self.direction.normalize()
-
-    # self.rect.center += self.direction * speed
-    self.hitbox.x += self.direction.x * speed
-    self.collision('horizontal')
-    self.hitbox.y += self.direction.y * speed
-    self.collision('vertical')
-    self.rect.center = self.hitbox.center
-
-  def collision(self, direction):
-    if direction == 'horizontal':
-      for sprite in self.obstacle_sprites:
-        if sprite.hitbox.colliderect(self.hitbox):
-          if self.direction.x > 0: # moving right
-            self.hitbox.right = sprite.hitbox.left
-          if self.direction.x < 0: # moving left
-            self.hitbox.left = sprite.hitbox.right
-
-    if direction == 'vertical':
-      for sprite in self.obstacle_sprites:
-        if sprite.hitbox.colliderect(self.hitbox):
-          if self.direction.y > 0: # moving down
-            self.hitbox.bottom = sprite.hitbox.top
-          if self.direction.y < 0: # moving up
-            self.hitbox.top = sprite.hitbox.bottom
 
   def cooldown(self):
     current_time = pygame.time.get_ticks()
