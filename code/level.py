e56b7177c643337d6365399b0f294b010a5952d8
John Turner <johnturner4004@gmail.com>
2023-06-12 11:34:32 -0500

    feat: insert placeholders for enemies

--- code/level.py
+++ code/level.py
@@ -7,6 +7,7 @@ from support import *
 from random import choice
 from weapon import Weapon
 from ui import UI
+from enemy import Enemy
 
 class Level:
   def __init__(self):
@@ -43,7 +44,8 @@ class Level:
     layouts = {
       'boundary': import_csv_layout('../map/map_FloorBlocks.csv'),
       'grass': import_csv_layout('../map/map_Grass.csv'),
-      'object': import_csv_layout('../map/map_Objects.csv')
+      'object': import_csv_layout('../map/map_Objects.csv'),
+      'entities': import_csv_layout('../map/map_Entities.csv')
     }
     graphics = {
       'grass': import_folder('../graphics/grass'),
@@ -64,15 +66,17 @@ class Level:
             if style == 'object':
               surface = graphics['object'][int(column)]
               Tile((x, y), [self.visible_sprites, self.obstacle_sprites], 'object', surface)
-
-
-    self.player = Player(
-      (2000,1430), 
-      [self.visible_sprites], 
-      self.obstacle_sprites, 
-      self.create_attack, 
-      self.destroy_attack,
-      self.create_magic)
+            if style == 'entities':
+              if column == '394':
+                self.player = Player(
+                  (x, y), 
+                  [self.visible_sprites], 
+                  self.obstacle_sprites, 
+                  self.create_attack, 
+                  self.destroy_attack,
+                  self.create_magic)
+              else:
+                Enemy('monster', (x, y), [self.visible_sprites])
 
   def create_attack(self):
     self.current_attack = Weapon(self.player, [self.visible_sprites])
