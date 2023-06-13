b540ce36b0f37b3b6bac668891011ac5dfba1b81
John Turner <johnturner4004@gmail.com>
2023-06-13 15:11:50 -0500

    feat: player damage and flicker

--- code/enemy.py
+++ code/enemy.py
@@ -4,7 +4,7 @@ from entity import Entity
 from support import *
 
 class Enemy(Entity):
-  def __init__(self, monster_name, pos, groups, obstacle_sprites):
+  def __init__(self, monster_name, pos, groups, obstacle_sprites, damage_player):
 
     # general setup
 
@@ -37,6 +37,7 @@ class Enemy(Entity):
     self.can_attack = True
     self.attack_time = None
     self.attack_cooldown = 400
+    self.damage_player = damage_player
 
     # invincibility duration
     self.vulnerable = True
@@ -80,7 +81,7 @@ class Enemy(Entity):
   def actions(self, player):
     if self.status == 'attack':
       self.attack_time = pygame.time.get_ticks()
-      print('attack')
+      self.damage_player(self.attack_damage, self.attack_type)
     elif self.status == 'move':
       self.direction = self.get_player_distance_direction(player)[1]
     else:
