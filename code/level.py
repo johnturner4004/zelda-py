3e1b6859c46059f623b15433ee6fe84647547689
John Turner <johnturner4004@gmail.com>
2023-06-13 14:02:46 -0500

    feat: able to attack enemies and grass

--- code/level.py
+++ code/level.py
@@ -100,6 +100,14 @@ class Level:
       self.current_attack.kill()
     self.current_attack = None
 
+  def player_attack_logic(self):
+    if self.attack_sprites:
+      for attack_sprite in self.attack_sprites:
+        collision_sprites = pygame.sprite.spritecollide(attack_sprite, self.attackable_sprites, True)
+        if collision_sprites:
+          for target_sprite in collision_sprites:
+            target_sprite.kill()
+
   def create_magic(self, style, strength, cost):
     print(style)
     print(strength)
@@ -110,6 +118,7 @@ class Level:
     self.visible_sprites.custom_draw(self.player)
     self.visible_sprites.update()
     self.visible_sprites.enemy_update(self.player)
+    self.player_attack_logic()
     self.ui.display(self.player)
 
 class YSortCameraGroup(pygame.sprite.Group):
