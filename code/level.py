d4701631161a396064bcd1665ad04037c8e1b31b
John Turner <johnturner4004@gmail.com>
2023-06-13 18:38:03 -0500

    feat: enemy death animations

--- code/level.py
+++ code/level.py
@@ -95,7 +95,8 @@ class Level:
                   (x, y), 
                   [self.visible_sprites, self.attackable_sprites], 
                   self.obstacle_sprites,
-                  self.damage_player)
+                  self.damage_player,
+                  self.trigger_death_particles)
 
   def create_attack(self):
     self.current_attack = Weapon(self.player, [self.visible_sprites, self.attack_sprites])
@@ -126,6 +127,10 @@ class Level:
       self.player.vulnerable = False
       self.player.hurt_time = pygame.time.get_ticks()
       self.animation_player.create_particles(attack_type, self.player.rect.center, [self.visible_sprites])
+
+  def trigger_death_particles(self, pos, particle_type):
+
+    self.animation_player.create_particles(particle_type, pos, self.visible_sprites)
 
   def create_magic(self, style, strength, cost):
     print(style)
