58f8c28abd10bf06747c1c3fe0fe96c9845b0a63
John Turner <johnturner4004@gmail.com>
2023-06-14 07:35:38 -0500

    feat: user can pause game

--- code/level.py
+++ code/level.py
@@ -10,12 +10,14 @@ from ui import UI
 from enemy import Enemy
 from particles import AnimationPlayer
 from magic import MagicPlayer
+from upgrade import Upgrade
 
 class Level:
   def __init__(self):
 
     # get the display surface
     self.display_surface = pygame.display.get_surface()
+    self.game_paused = False
 
     # sprite group setup
     self.visible_sprites = YSortCameraGroup()
@@ -31,6 +33,7 @@ class Level:
 
     # user interface
     self.ui = UI()
+    self.upgrade = Upgrade(self.player)
 
     # particles
     self.animation_player = AnimationPlayer()
@@ -149,13 +152,22 @@ class Level:
   def add_xp(self, amount):
     self.player.exp += amount
 
+  def toggle_menu(self):
+    self.game_paused = not self.game_paused
+
   def run(self):
-    # update and draw the game
+    # always visible
     self.visible_sprites.custom_draw(self.player)
-    self.visible_sprites.update()
-    self.visible_sprites.enemy_update(self.player)
-    self.player_attack_logic()
     self.ui.display(self.player)
+
+    if self.game_paused:
+      # upgrade menu
+      self.upgrade.display()
+    else: 
+      # visible when game is running
+      self.visible_sprites.update()
+      self.visible_sprites.enemy_update(self.player)
+      self.player_attack_logic()
 
 class YSortCameraGroup(pygame.sprite.Group):
   def __init__(self):
