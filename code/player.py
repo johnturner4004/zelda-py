e0a9506810a2c00c4f533603e2240bcc4401ea34
John Turner <johnturner4004@gmail.com>
2023-06-14 10:26:22 -0500

    feat: weapon sound effects

--- code/player.py
+++ code/player.py
@@ -71,6 +71,10 @@ class Player(Entity):
     self.hurt_time = None
     self.invulnerability_duration = 500
 
+    # import sounds
+    self.weapon_attack_sound = pygame.mixer.Sound('../audio/sword.wav')
+    self.weapon_attack_sound.set_volume(0.4)
+
   def import_player_assets(self):
     character_path = '../graphics/player/'
 
@@ -112,6 +116,7 @@ class Player(Entity):
         self.attacking = True
         self.attack_time = pygame.time.get_ticks()
         self.create_attack()
+        self.weapon_attack_sound.play()
 
       # magic input
       if keys[pygame.K_f]:
